160b33abbbd90263d6d3ebd05ecab3f2305ca386
ChrisStyles99 <chrisrdz99@hotmail.com>
2020-11-27 22:11:25 -0600

    Changed variables

--- classes/DB.php
+++ classes/DB.php
@@ -3,12 +3,6 @@
   include('.env.php');
 
   class DB {
-    private $host = 'localhost';
-    private $user = 'root';
-    private $password = 'wicho1';
-    private $dbName = 'php_inventory';
-    private $port = 3307;
-
     protected function connect() {
       $dsn = "mysql:host=". $_ENV['DB_HOST'] .";dbname=". $_ENV['DB_NAME'] .";port=" . $_ENV['DB_PORT'];
       $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
